3ce57a4e4df2940d8f134176e176bffa1550647c
Jeong seulho <wjdtmfgh@gmail.com>
2023-10-10 22:42:12 +0900

    [level 3] Title: 단속카메라, Time: 1.00 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,12 +1,10 @@
 def solution(routes):
     routes.sort(key = lambda x : x[1])
-    print(routes)
     ans = 1
     last_end = routes[0][1]
     for i in range(1, len(routes)):
         cur_start = routes[i][0]
         if last_end < cur_start:
-            print(last_end, cur_start)
             ans += 1
             last_end = routes[i][1]
         
