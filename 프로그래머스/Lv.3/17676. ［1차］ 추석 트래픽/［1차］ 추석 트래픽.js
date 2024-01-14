f08b0fb29daa91df170c9d3c2e5e7d39334bc6ce
Jeong seulho <wjdtmfgh@gmail.com>
2024-01-14 19:09:18 +0900

    [level Lv.3] Title: [1차] 추석 트래픽, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.js
+++ 프로그래머스/Lv.3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.js
@@ -1,35 +1,34 @@
-const transTimeToMilliSeconds = (time) => {
-  const hour = (time[0]*10 + time[1]*1) * 60 * 60;
-  const minute = (time[3]*10 + time[4]*1) * 60;
-  const seconds = time[6]*10 + time[7]*1;
-  const millis = time[9]*100 + time[10]*10 + time[11]*1;
-  const amount = (hour + minute + seconds) * 1000 + millis;
-  
-  return amount;
-}
-
 function solution(lines) {
-    const times = []
-    for(const line of lines) {
-        const [date, completeTime, duration] = line.split(' ');
-        const completeMillis = transTimeToMilliSeconds(completeTime)
-        const endTime = completeMillis + 999;
-        const startTime = completeMillis - duration.substr(0, duration.length-1)*1000 + 1;
-        
-        times.push(['START', startTime]);
-        times.push(['END', endTime]);
-    }
+  var answer = 0;
+  let times =[];
 
-    times.sort((a, b) => a[1] !== b[1] ? a[1] - b[1] : -1);
-    
-    let answer = 0;
-    let count = 0;
-    for(const time of times) {
-      if(time[0] === 'START') count++;
-      else count--;
+  lines.map(str => str.split(' ')).forEach(e =>{
+    const hour = Number(e[1].split(':')[0]) * 3600 * 1000; // 시간을 밀리초로 변경
+    const minute = Number(e[1].split(':')[1]) * 60 * 1000; // 분을 밀리초로 변경
+    const second = Number(e[1].split(':')[2]) * 1000;      // 초를 밀리초로 변경
 
-      answer = Math.max(answer, count);
-    }
+    let end = hour + minute + second; // 끝시간 
+    let pass = Number(e[2].slice(0,-1)) * 1000; // 처리시간을 밀리초로 변경
+    let start = end - pass +1; // 처리시간은 시작시간과 끝시간을 포함하므로 +1 추가
 
-    return answer;
+    times.push(['START', start]);
+    times.push(['END', end+1000]); // 1초동안 최대처리량을 알기위해 1000ms 추가
+
+  });
+
+  // 시간을 오름차순으로 나열
+  times.sort((a,b) => {
+    return a[1] - b[1] < 0 ? -1 : 1;
+  })
+
+  // START일때 카운트 +1 , END일때 카운트된 최대처리량 확인 후 카운트 -1
+  let count = 0;
+  times.forEach(e => {
+    e[0] === 'START' ? count++ : (
+      answer = answer > count ? answer : count,
+      count--
+    )
+  })
+
+  return answer;
 }
