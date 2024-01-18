80fe8218bbc2e8ccdda50e790f4e5080e9867223
Jeong seulho <wjdtmfgh@gmail.com>
2024-01-18 13:29:32 +0900

    Delete 프로그래머스 directory

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++
@@ -1,9 +0,0 @@
-function solution(nums) {
-  const selectCnt = nums.length / 2;
-  const setNums = new Set(nums);
-  if (setNums.size > selectCnt) {
-    return selectCnt;
-  } else {
-    return setNums.size;
-  }
-}
