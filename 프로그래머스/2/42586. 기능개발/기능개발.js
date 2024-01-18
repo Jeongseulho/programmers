54c4c990b13bb424295efc242aab5b121b05ec86
Jeong seulho <wjdtmfgh@gmail.com>
2024-01-18 13:29:45 +0900

    [level 2] Title: 기능개발, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,27 @@
+function solution(progresses, speeds) {
+    const ans = [];
+    
+    let checkIdx = 0;
+    while (true) {
+        
+        if (progresses.every((ele) => ele >= 100)) break;
+        
+        progresses.forEach((ele, idx, arr) => {
+            arr[idx] += speeds[idx]
+        })
+        
+        let cnt = 0;
+        for (let i = checkIdx; i < progresses.length; i++) {
+            if (progresses[i] >= 100) {
+                cnt++;
+            }
+            else {
+                checkIdx = i;
+                break;
+            }
+        }
+        
+        if (cnt !== 0) ans.push(cnt);
+    }
+    return ans;
+}
