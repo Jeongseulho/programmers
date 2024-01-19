2cd7c73ea0b2e275483bfdeb90b919b24ecc1134
Jeong seulho <wjdtmfgh@gmail.com>
2024-01-19 10:34:48 +0900

    [level 2] Title: 더 맵게, Time: 582.62 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -0,0 +1,75 @@
+function shiftDown(heap) {
+  let targetIdx = 0;
+  let leftChildIdx = 1;
+  let rightChildIdx = 2;
+  let smallerChildIdx = leftChildIdx;
+  while (smallerChildIdx < heap.length) {
+    if (
+      rightChildIdx < heap.length &&
+      heap[rightChildIdx] < heap[leftChildIdx]
+    ) {
+      smallerChildIdx = rightChildIdx;
+    } else {
+      smallerChildIdx = leftChildIdx;
+    }
+
+    if (heap[smallerChildIdx] >= heap[targetIdx]) break;
+
+    [heap[targetIdx], heap[smallerChildIdx]] = [
+      heap[smallerChildIdx],
+      heap[targetIdx],
+    ];
+    targetIdx = smallerChildIdx;
+    leftChildIdx = targetIdx * 2 + 1;
+    rightChildIdx = leftChildIdx + 1;
+    smallerChildIdx = leftChildIdx;
+  }
+}
+
+function minHeapPush(minHeap, item) {
+    minHeap.push(item);
+    let targetIdx = minHeap.length - 1;
+    
+    while(targetIdx > 0) {
+        const parIdx = (targetIdx - 1) >> 1;
+        const par = minHeap[parIdx];
+        if (item < par) {
+            minHeap[targetIdx] = par;
+            targetIdx = parIdx;
+            continue;
+        }
+        break;
+    }
+    minHeap[targetIdx] = item;
+}
+
+function minHeapPop(heap) {
+  const lastItem = heap.pop();
+  if (heap.length) {
+    const minItem = heap[0];
+    heap[0] = lastItem;
+    shiftDown(heap);
+    return minItem;
+  }
+  return lastItem;
+}
+
+function solution(scoville, K) {
+    const minHeap = [];
+    for (const food of scoville) {
+        minHeapPush(minHeap, food);
+    }
+    let cnt = 0;
+    while(minHeap[0] < K) {
+        if (minHeap.length === 1) {
+            return -1;
+        }
+        const curFood = minHeapPop(minHeap);
+        const nextFood = minHeapPop(minHeap);
+        const mixedFood = curFood + nextFood * 2
+        minHeapPush(minHeap, mixedFood);
+        cnt += 1;
+    }
+    
+    return cnt;
+}
