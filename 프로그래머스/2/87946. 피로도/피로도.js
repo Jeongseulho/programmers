2eaf30e600a9ab24ac1fa11066c313405afcf90d
Jeong seulho <wjdtmfgh@gmail.com>
2024-01-24 08:59:53 +0900

    [level 2] Title: 피로도, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,20 +1,20 @@
 function solution(life, dungeons) {
-  const N = dungeons.length;
-  const visited = new Array(N).fill(0);
-  let ans = 0;
-
-  function dfs(life, cnt) {
-    ans = Math.max(cnt, ans);
-
-    for (let j = 0; j < N; j++) {
-      if (life >= dungeons[j][0] && !visited[j]) {
-        visited[j] = 1;
-        dfs(life - dungeons[j][1], cnt + 1);
-        visited[j] = 0;
-      }
+    const N = dungeons.length;
+    const visited = new Array(N).fill(0);
+    let ans = 0;
+    
+    function dfs(life, cnt) {
+        ans = Math.max(ans, cnt);
+        
+        for (let i = 0; i < N; i++) {
+            if (!visited[i] && life >= dungeons[i][0]) {
+                visited[i] = 1;
+                dfs(life - dungeons[i][1], cnt + 1)
+                visited[i] = 0;
+            }
+        }
     }
-  }
-
-  dfs(life, 0);
-  return ans;
+    
+    dfs(life, 0);
+    return ans;
 }
