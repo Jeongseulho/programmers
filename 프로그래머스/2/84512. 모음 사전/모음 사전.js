b80ba656147cbcf53c2404c2241462975b8571c6
Jeong seulho <wjdtmfgh@gmail.com>
2024-01-24 13:49:26 +0900

    [level 2] Title: 모음 사전, Time: 0.88 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,18 +1,27 @@
 function solution(word) {
-  const dict = [];
-  for (let len = 1; len < 6; len++) dfs(0, dict, '', len);
-  dict.sort();
-  return dict.indexOf(word) + 1;
-}
-
-function dfs(n, dict, word, len) {
-  const alphas = ['A', 'E', 'I', 'O', 'U'];
-
-  if (n === len) {
-    dict.push(word);
-    return;
-  }
-  alphas.forEach((alpha) => {
-    dfs(n + 1, dict, word + alpha, len);
-  });
+    let ans = 0;
+    const alpha = ['A', 'E', 'I', 'O', 'U'];
+    let cnt = 0;
+    let isFindAns = false;
+    
+    function dfs(str) {
+        if (isFindAns || str.length > 5) {
+            return;
+        }
+        
+        if (str === word) {
+            isFindAns = true;
+            ans = cnt;
+            return;
+        }
+        
+        cnt++;
+        
+        for (let i = 0; i < alpha.length; i++) {
+            dfs(str + alpha[i])
+        }
+    }
+    
+    dfs("");
+    return ans;
 }
