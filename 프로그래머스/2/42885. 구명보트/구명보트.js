7f4a9343a666ab203ef05a48e0c7b53935f4e433
Jeong seulho <wjdtmfgh@gmail.com>
2024-01-25 12:51:34 +0900

    [level 2] Title: 구명보트, Time: 39.10 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,19 @@
+function solution(people, limit) {
+    people.sort((a, b) => a - b);
+    let startIdx = 0;
+    let endIdx = people.length - 1;
+    let cnt = 0;
+    
+    while (startIdx <= endIdx) {
+        const sum = people[startIdx] + people[endIdx];
+        
+        if (sum <= limit) {
+            startIdx++;
+        }
+        
+        endIdx--;
+        cnt++;
+    }
+    
+    return cnt;
+}
