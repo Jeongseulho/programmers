81988f6bdb947e482520090e8b6178775b625df1
Jeong seulho <wjdtmfgh@gmail.com>
2024-02-07 09:08:03 +0900

    [level 3] Title: 입국심사, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -0,0 +1,13 @@
+function solution(n, times) {
+    let minTime = 1;
+    let maxTime = Math.max(...times) * n;
+    
+    while (minTime <= maxTime) {
+        const midTime = Math.floor((minTime + maxTime) / 2);
+        const peopleCnt = times.reduce((acc, spentTime) => acc + Math.floor(midTime / spentTime), 0);
+              
+        if (peopleCnt >= n) maxTime = midTime - 1;
+        else minTime = midTime + 1;
+    }
+    return minTime;
+}
