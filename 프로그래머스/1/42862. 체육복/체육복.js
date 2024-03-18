be64a08630275050c74bfe87a8dcb728ae10f065
Jeong seulho <wjdtmfgh@gmail.com>
2024-03-18 20:21:22 +0900

    [level 1] Title: 체육복, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,5 +1,6 @@
 function solution(n, lost, reserve) {
-    const clothesCnt = new Array(n + 1).fill().map((ele, idx) => idx === 0 ? null : 1)
+    const clothesCnt = new Array(n + 1).fill(1);
+    clothesCnt[0] = -1;
     lost.forEach((student) => clothesCnt[student] -= 1 );
     reserve.forEach((student) => clothesCnt[student] += 1);
     
@@ -17,6 +18,6 @@ function solution(n, lost, reserve) {
             }
         }
     }
-    console.log(clothesCnt)
-    return clothesCnt.reduce((acc, cur) => cur > 0 ? acc + 1 : acc, 0)
+    
+    return clothesCnt.filter((ele) => ele > 0).length;
 }
