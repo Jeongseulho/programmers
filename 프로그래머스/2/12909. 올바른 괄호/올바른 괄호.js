0b2237906d9c8ae1b3e035d4d8c021c0cd2161bb
Jeong seulho <wjdtmfgh@gmail.com>
2024-03-22 09:40:59 +0900

    [level 2] Title: 올바른 괄호, Time: 28.74 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -2,15 +2,15 @@ function solution(s){
     const stack = [];
     for (const str of s) {
         if (str === "(") {
-            stack.push(str)
-        } else {
-            if (stack.length === 0 || stack.pop() !== "(") {
-                return false
+            stack.push(str);
+        }
+        else {
+            if (stack[stack.length - 1] === "(") {
+                stack.pop();
+            } else {
+                return false;
             }
         }
     }
-    if (stack.length) {
-        return false
-    }
-    return true
+    return stack.length === 0 ? true : false;
 }
