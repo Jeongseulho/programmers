aa1bbb5bd862651d16698316a675243a65e7219a
Jeong seulho <wjdtmfgh@gmail.com>
2024-03-29 00:41:39 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -1,34 +1,18 @@
-function isPrime(n) {
-  if (n < 2) {
-    return false;
-  }
-  for (let i = 2; i < parseInt(n ** 0.5) + 1; i++) {
-    if (n % i === 0) {
-      return false;
+const isPrime = (num) => {
+    if (num < 2) return false;
+    for (let i = 2; i < parseInt(num ** 0.5) + 1; i++) {
+        if (num % i === 0) return false;
     }
-  }
-  return true;
-}
-
-function convert(n, base) {
-  const ans = [];
-  while (n > 0) {
-    ans.push(n % base);
-    n = parseInt(n / base);
-  }
-  return ans.reverse().join('');
+    return true;
 }
 
 function solution(n, k) {
-  let cnt = 0;
-  const k진법의수 = convert(n, k);
-  const nums = k진법의수.split('0');
-  for (const num of nums) {
-    if (num && isPrime(parseInt(num))) {
-      cnt += 1;
-    }
-  }
-
-  return cnt;
+    const convertNum = n.toString(k);
+    const numList  = convertNum.split('0');
+    let cnt = 0;
+    numList.forEach((num) => {
+        if (num && isPrime(Number(num))) cnt += 1;
+    })
+    return cnt;
 }
 
