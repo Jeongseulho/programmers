86bed5baeda150c04b26f3eaae8f47c0d16b9396
Jeong seulho <wjdtmfgh@gmail.com>
2024-04-01 15:25:33 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.02 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -3,15 +3,14 @@ function solution(maps) {
     const maxJ = maps[0].length - 1;
     const needVisit = [[0, 0, 1]];
     
-    // 상하좌우
     const di = [-1, 1, 0, 0];
     const dj = [0, 0, -1, 1];
     
-    while(needVisit.length) {
+    while (needVisit.length) {
         const [ci, cj, cnt] = needVisit.shift();
         if (ci === maxI && cj === maxJ) return cnt;
-
-        for (let i = 0; i < 4; i++) {
+        
+        for (let i = 0; i < 5; i++) {
             const ni = ci + di[i];
             const nj = cj + dj[i];
             if (0 <= ni && ni <= maxI && 0 <= nj && nj <= maxJ && maps[ni][nj]) {
