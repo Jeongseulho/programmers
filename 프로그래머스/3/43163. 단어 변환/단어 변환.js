7c0076e469a9c9fd83614ab7bc69d3c0381cfe5f
Jeong seulho <wjdtmfgh@gmail.com>
2024-04-03 13:16:38 +0900

    [level 3] Title: 단어 변환, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,29 +1,25 @@
-const isDiffOneLetter = (word1, word2) => {
+const isDiffOneLetter = (curWord, nextWord) => {
     let diffCnt = 0;
-    for (let i = 0; i < word1.length; i++) {
-        if (word1[i] !== word2[i]) {
-            diffCnt++;
-            if (diffCnt > 1) return false;
-        }
+    curWord = curWord.split('');
+    nextWord = nextWord.split('');
+    for (let i = 0; i < curWord.length; i++) {
+        if (curWord[i] !== nextWord[i]) diffCnt += 1;
     }
     return diffCnt === 1 ? true : false;
 }
 
 function solution(begin, target, words) {
     const needVisit = [[begin, 0]];
-    const visited = [];
-    
+    const visited = new Array(words.length).fill(false);
     while (needVisit.length) {
-        const [word, cnt] = needVisit.shift();
-        if (word === target) return cnt;
-        if (!visited.includes(word)) {
-            visited.push(word);
-            words.forEach((ele) => {
-                if(isDiffOneLetter(ele, word)) {
-                    needVisit.push([ele, cnt + 1]);   
-                }
-            })
-        }
+        const [ curWord, step ] = needVisit.shift();
+        if (curWord === target) return step;
+        words.forEach((word, idx) => {
+            if (!visited[idx] && isDiffOneLetter(curWord, word)) {
+                needVisit.push([ word, step + 1]);
+                visited[idx] = true;
+            }
+        })
     }
     return 0;
 }
