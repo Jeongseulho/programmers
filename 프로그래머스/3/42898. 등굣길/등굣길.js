32666f3bfde318bb7ac2be623935ef11fa0d7bcd
Jeong seulho <wjdtmfgh@gmail.com>
2024-04-03 14:09:40 +0900

    [level 3] Title: 등굣길, Time: 4.06 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,20 +1,24 @@
 function solution(m, n, puddles) {
-  const MOD = 1000000007;
-  const dp = Array.from({ length: n }, () => Array(m).fill(0));
+    const MOD = 1000000007;
+    const dp = Array.from({ length: n }, () => Array(m).fill(0));
 
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-      if (i === 0 && j === 0) {
-          dp[0][0] = 1;
-          continue;
-      }
-        
-      if (puddles.some(([x, y]) => x === j + 1 && y === i + 1)) {
-        dp[i][j] = 0;
-      } else {
-        dp[i][j] = ((i > 0 ? dp[i - 1][j] : 0) + (j > 0 ? dp[i][j - 1] : 0)) % MOD;
-      }
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            
+            if (i === 0 && j === 0) {
+                dp[0][0] = 1;
+                continue;
+            }
+            
+            const isPuddle = puddles.some(([x, y]) => x === j + 1 && y === i + 1)
+            if (isPuddle) {
+                dp[i][j] = 0;
+            } else {
+                const up = i > 0 ? dp[i - 1][j] : 0;
+                const down = (j > 0 ? dp[i][j - 1] : 0);
+                dp[i][j] = (up + down) % MOD;
+            }
+        }
     }
-  }
-  return dp[n - 1][m - 1];
+    return dp[n - 1][m - 1];
 }
