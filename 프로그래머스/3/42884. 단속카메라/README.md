754fd914791e4805f0480026eb4ed3ff75aae53f
Jeong seulho <wjdtmfgh@gmail.com>
2024-04-08 21:48:48 +0900

    [level 3] Title: 단속카메라, Time: 3.38 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/README.md
+++ 프로그래머스/3/42884. 단속카메라/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단속카메라 - 42884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884) 
 
 ### 성능 요약
 
-메모리: 36.7 MB, 시간: 3.08 ms
+메모리: 36.8 MB, 시간: 3.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 13:28:22
+2024년 04월 08일 21:48:44
 
 ### 문제 설명
 
--- 프로그래머스/3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -1,15 +1,15 @@
 function solution(routes) {
     routes.sort((a, b) => a[1] - b[1]);
-    let camPos = routes[0][1];
-    let ans = 1;
+    let cnt = 1;
+    let lastCamPos = routes[0][1];
     
     for (const route of routes) {
         const [start, end] = route;
-        if (start <= camPos && camPos <= end) {
-            continue;
+        if (lastCamPos < start || lastCamPos > end) {
+            lastCamPos = end;
+            cnt += 1;
         }
-        camPos = end;
-        ans++;
     }
-    return ans;
+    
+    return cnt;
 }
