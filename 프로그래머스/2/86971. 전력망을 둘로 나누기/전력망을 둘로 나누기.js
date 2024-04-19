f3165491f227282004d1884d3b0586fa7f02bd8a
Jeong seulho <wjdtmfgh@gmail.com>
2024-04-19 10:16:21 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 7.24 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,37 +1,28 @@
-const getNodeCount = (adjList) => {
-    const visited = [];
-    let needVisit = [1];
-    
-    while(needVisit.length) {
-        const node = needVisit.pop();
-        const adjNodeList = adjList[node];
-        for(let i = 0; i < adjNodeList.length; i++) {
-            const adjNode = adjNodeList[i];
-            if(!visited.includes(adjNode)) {
-                needVisit.push(adjNode);
-                visited.push(adjNode);
-            }
-        }
-    }
-    
-    return visited.length;
-}
-
 function solution(n, wires) {
     let ans = 100;
-    for (let disConnectWire = 0; disConnectWire < wires.length; disConnectWire++) {
-        const adjList = new Array(n + 1).fill(null).map(() => []);
-        for (let i = 0; i < wires.length; i++) {
-            if (i === disConnectWire) continue;
-            const [v1, v2] = wires[i];
+    for (let delWireIdx = 0; delWireIdx < wires.length; delWireIdx++) {
+        const delWires = wires.filter((_, idx) => idx !== delWireIdx)
+        const adjList = Array.from({length : n + 1}, () => []);
+        delWires.forEach(([v1, v2]) => {
             adjList[v1].push(v2);
             adjList[v2].push(v1);
-        }
+        })
         
-        const cnt = getNodeCount(adjList);
-        const otherCnt = n - cnt
-        const diff = Math.abs(cnt - otherCnt)
-        ans = Math.min(diff, ans)
+        const visited = [delWires[0][0]];
+        const needVisit = [delWires[0][0]];
+        
+        while (needVisit.length) {
+            const node = needVisit.pop();
+            adjList[node].forEach((adjNode) => {
+                if (!visited.includes(adjNode)) {
+                    visited.push(adjNode);
+                    needVisit.push(adjNode);
+                }
+            })
+        }
+        const otherCnt = n - visited.length;
+        const diff = Math.abs(otherCnt - visited.length)
+        ans = Math.min(diff, ans);
     }
     return ans;
 }
