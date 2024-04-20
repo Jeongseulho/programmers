c8dee56ad5d6890aecc901b7734aed9e9d435f12
Jeong seulho <wjdtmfgh@gmail.com>
2024-04-20 20:40:42 +0900

    [level 3] Title: 여행경로, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,6 +1,6 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 17:20:47
+2024년 04월 20일 20:40:38
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -1,21 +1,17 @@
 function solution(tickets) {
-    const ansList = [];
-    const dfs = (nowPlace, remainTickets, path) => {
-        if (remainTickets.length === 0) {
-            ansList.push(path);
-            return;
-        };
+    const pathList = [];
+    
+    const dfs = (curPlace, remainTickets, path) => {
+        if (!remainTickets.length) return pathList.push(path);
         
-        remainTickets.forEach((ticket, idx, tickets) => {
-            const [from, to] = ticket;
-            
-            if (from !== nowPlace) return;
-            
-            const nextRemianTickets = tickets.filter((_, nextTicketIdx) => nextTicketIdx !== idx)
-            dfs(to, nextRemianTickets, [...path, to])
+        remainTickets.forEach(([from, to], idx) => {
+            if (curPlace !== from) return;
+            const nextRemainTickets = remainTickets.filter((_, i) => i !==idx );
+            dfs(to, nextRemainTickets, [...path, to]);
         })
     }
     
-    dfs("ICN", tickets, ["ICN"]);
-    return ansList.sort()[0];
+    dfs('ICN', tickets, ['ICN']);
+    
+    return pathList.sort()[0];
 }
