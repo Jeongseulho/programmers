7fd7f792301b92d1b16a20447c19300fca389b80
Jeong seulho <wjdtmfgh@gmail.com>
2024-04-20 15:18:53 +0900

    [level 3] Title: 섬 연결하기, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
@@ -1,34 +1,32 @@
-const getParent = (parent, x) => {
-  if(parent[x] === x) return x;
-  return parent[x] = getParent(parent, parent[x]);
-}
 
-const unionParent = (parent, a, b) => {
-  const n1 = getParent(parent, a);
-  const n2 = getParent(parent, b);
-  if(n1 < n2) return parent[n2] = n1;
-  else return parent[n1] = n2;
-}
-
-const isSameParent = (parent, a, b) => {
-  const n1 = getParent(parent, a);
-  const n2 = getParent(parent, b);
-  if(n1 === n2) return true;
-  else return false;
-}
 
 function solution(n, costs) {
-  let answer = 0;
-  const parent = Array.from({length : n}, (_, idx) => idx );
-  
-  costs.sort((a, b) => a[2] - b[2]);
-  
-  for(const cost of costs) {
-    if(!isSameParent(parent, cost[0], cost[1])) {
-      answer += cost[2];
-      unionParent(parent, cost[0], cost[1]);
+    let minCost = 0;
+    const parent = Array.from({length : n}, (_, idx) => idx );
+    
+    const getParent = (x) => {
+      if(parent[x] === x) return x;
+      return parent[x] = getParent(parent[x]);
     }
-  }
+
+    const unionParent = (a, b) => {
+      const n1 = getParent(a);
+      const n2 = getParent(b);
+      n1 < n2 ? (parent[n2] = n1) : (parent[n1] = n2);
+    }
+
+    const isSameParent = (a, b) => {
+      return getParent(a) === getParent(b);
+    }
   
-  return answer;
+    costs.sort((a, b) => a[2] - b[2]);
+  
+    for(const [from, to, cost] of costs) {
+        if(!isSameParent(from, to)) {
+        minCost += cost;
+        unionParent(from, to);
+        }
+    }
+  
+  return minCost;
 }
