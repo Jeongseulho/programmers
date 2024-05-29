6fd377c92e100d1cd3f82cb6a95f8d7f0e912dcf
Jeong seulho <wjdtmfgh@gmail.com>
2024-05-29 23:39:07 +0900

    [level 2] Title: [1차] 캐시, Time: 1.97 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,16 +1,13 @@
 function solution(cacheSize, cities) {
     let cache = [];
-    let ans = 0;
-    for (let city of cities) {
+    let time = 0;
+    for(let city of cities) {
         city = city.toUpperCase();
-        if (cache.includes(city)) {
-            ans += 1;
-        } else {
-            ans += 5;
-        }
-        cache = cache.filter((cachedCity) => cachedCity !== city)
-        cache.push(city);
-        if (cache.length > cacheSize) cache.shift();
+        if(cache.includes(city)) time += 1;
+        else time += 5;
+        cache = cache.filter((cachedCity) => cachedCity !== city);
+        cache.unshift(city);
+        if(cache.length > cacheSize) cache.pop();
     }
-    return ans;
+    return time;
 }
