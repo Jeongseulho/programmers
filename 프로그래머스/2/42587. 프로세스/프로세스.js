e14cef6133583b4f2fcf3b28f1b240f5b7bd2940
Jeong seulho <wjdtmfgh@gmail.com>
2024-07-21 22:23:20 +0900

    [level 2] Title: 프로세스, Time: 0.68 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,18 +1,13 @@
 function solution(priorities, location) {
-    const Q = Array.from({length:priorities.length}, (_,i) => i); 
-    
+    const queue = priorities.map((priorty, idx) => ({loc : idx , priorty}));
     let order = 0;
-    while (Q.length) {
-        const process = Q.shift();
-        const processPriority = priorities[process]
-        if (Q.some((process) => priorities[process] > processPriority)) {
-            Q.push(process);
-        } else {
-            order++;
-            if (process === location) {
-                return order;
-            }
+    
+    while(queue.length) {
+        const process = queue.shift();
+        if(queue.some(({_, priorty}) => priorty > process.priorty)) queue.push(process);
+        else {
+            order += 1;
+            if(process.loc === location) return order;
         }
-            
     }
 }
