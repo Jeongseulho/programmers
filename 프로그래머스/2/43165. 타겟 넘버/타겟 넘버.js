674c1392eadff01f8a4fee36a783af8092c86e76
Jeong seulho <wjdtmfgh@gmail.com>
2024-07-23 14:48:04 +0900

    [level 2] Title: 타겟 넘버, Time: 14.72 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,15 +1,15 @@
 function solution(numbers, target) {
     let ans = 0;
-    const count = (sum, idx) => {
-        if (idx === numbers.length) {
-            if (sum === target) ans += 1;
+    const dfs = (sum, idx) => {
+        if(idx === numbers.length) {
+            if(sum === target) ans += 1;
             return;
         }
         
-        count(sum + numbers[idx], idx + 1);
-        count(sum - numbers[idx], idx + 1);
+        dfs(sum - numbers[idx], idx + 1);
+        dfs(sum + numbers[idx], idx + 1);
     }
     
-    count(0, 0);
+    dfs(0, 0);
     return ans;
 }
