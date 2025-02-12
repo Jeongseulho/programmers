5678de2f1d625d34ebc6980b31359b4ddf85ac32
Jeong seulho <wjdtmfgh@gmail.com>
2025-02-12 21:24:23 +0900

    [level 1] Title: 예산, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,12 +1,11 @@
 function solution(d, budget) {
     d.sort((a, b) => a - b);
-    
     let cnt = 0;
-    for(const money of d) {
-        budget -= money;
-        if(budget < 0) return cnt;
-        cnt += 1;
+    for(const req of d) {
+        budget -= req;
+        if(budget < 0) break;
+        else cnt += 1;
     }
     
-    return d.length;
+    return cnt;
 }
